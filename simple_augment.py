e600df898e12a5476845eed2385d2ffe17be1afa
Alif Aditya W <81549262+Adddedtiya@users.noreply.github.com>
2021-11-17 22:46:01 +0700

    Working And BugFixed

--- simple_augment.py
+++ simple_augment.py
@@ -1,7 +1,8 @@
 from file_loader import AnnotationException, load_File, Write_File
-from aug_seq import AugmentationSequence, AugmentationSequence2
+from aug_seq import AugmentationSequence
 import argparse
 import os
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Throw Eggs on the wall image augmentation')
 parser.add_argument('--indir', type=str, required=True)
@@ -38,14 +39,13 @@ print("Total Image Anotation Pair :", len(DatasetTuple))
 if input("Y/N : ").lower() != "y":
     exit()
 
-for img, ann in (DatasetTuple):
+for img, ann in tqdm(DatasetTuple):
     try:
         img_raw, ann_raw = load_File(img, ann)
     except AnnotationException as e:
         print(e)
         continue
     else:
-        
         rootfname = os.path.basename(img).split('.')[0]
         
         combined_image_path = os.path.join(args.outdir, rootfname)
@@ -54,14 +54,19 @@ for img, ann in (DatasetTuple):
         Write_File(img_raw, ann_raw, combined_image_path + ".jpg", combined_annotation_path + ".txt")
         
         for i in range(args.gen):
-            print("Generating :", str(i).zfill(3), rootfname) if i % 10 == 1 else 0
+            #print("Generating :", str(i).zfill(3), rootfname) if i % 10 == 1 else 0
             
-            aug_img, aug_annot = AugmentationSequence2(image = img_raw, bounding_boxes = ann_raw)
+            aug_img, aug_annot = AugmentationSequence(image = img_raw, bounding_boxes = ann_raw)
+            aug_annot = aug_annot.remove_out_of_image().clip_out_of_image()
+            
+            if aug_annot.empty:
+                continue
             
             img_out_path = combined_image_path + "_" + str(i).zfill(3) + ".jpg"
             aug_out_path = combined_annotation_path + "_" + str(i).zfill(3) + ".txt"
             
             Write_File(aug_img, aug_annot, img_out_path, aug_out_path)
+            del aug_img, aug_annot
 
 
 
